81cf93c9971de88c88946193af2746a6c8b12137
Axtam-01 <axtam523@gmail.com>
2025-04-12 21:07:28 +0500

    I added a search button.

--- src/components/searchCity.vue
+++ src/components/searchCity.vue
@@ -8,6 +8,12 @@
         placeholder="Search for cities"
         class="search"
       />
+      <button @click="submitCity" class="searchBtn">
+        <img
+          src="/src/assets/img/search_24dp_FFF_FILL0_wght400_GRAD0_opsz24 (1).svg"
+          alt="searchImg"
+        />
+      </button>
     </section>
   </div>
 </template>
@@ -36,8 +42,20 @@ const submitCity = () => {
   border: none;
   padding-left: 0.9rem;
   outline: none;
+  padding-left: 40px;
 }
 .search::placeholder {
-  color: white;
+  color: rgb(138, 131, 131);
+}
+.weatherNav {
+  display: flex;
+  align-items: center;
+}
+.searchBtn {
+  background: none;
+  border: none;
+  position: absolute;
+  left: 260px;
+  cursor: pointer;
 }
 </style>
